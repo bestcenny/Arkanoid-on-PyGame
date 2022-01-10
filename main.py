36bd03cf6c1aebc0f41f0c27b585fdaa58564bdc
bestcenny <52035479+bestcenny@users.noreply.github.com>
2022-01-10 20:00:56 +0300

    Создание класса Paddle, отрисовка платформы и её управление

--- main.py
+++ main.py
@@ -23,6 +23,41 @@ class Ball:
         pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
 
 
+class Paddle:
+    # (self, game_screen: pygame.Surface, coord_x=568, coord_y=718,
+
+    def __init__(self):
+        self.paddle_image = pygame.image.load('paddleRed.png')
+        self.rect = self.paddle_image.get_rect()
+        self.x = 568
+        self.y = 718
+
+    # перемещение платформы по нажатию
+    def move(self):
+        key = pygame.key.get_pressed()
+        speed = 10
+        if key[pygame.K_RIGHT]:  # нажатие правой стрелки
+            if (self.x + self.rect[2]) + speed >= SIZE[0]:
+                self.x = SIZE[0] - self.rect[2]
+            else:
+                self.x += speed
+        elif key[pygame.K_LEFT]:  # нажатие левой стрелки
+            if self.x - speed <= 0:
+                self.x = 0
+            else:
+                self.x -= speed
+
+    def draw(self):
+        screen.blit(self.paddle_image, (self.x, self.y))
+
+
+SIZE = (1200, 750)
+paddle = Paddle()
+
+
+
+
+
 if __name__ == '__main__':
     pygame.init()
     size = (1200, 750)
@@ -39,9 +74,13 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
 
+        paddle.move()
         # обновление экрана
         screen.blit(background_image, (0, 0))
+        paddle.draw()
         pygame.display.flip()
         clock.tick(fps)
 
