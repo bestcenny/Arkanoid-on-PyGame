7b1e38aa88f60e4328dab4b6286341864ebb9f78
bestcenny <52035479+bestcenny@users.noreply.github.com>
2022-01-10 19:58:20 +0300

    Добавление класса Ball, отрисовка и упрощенная физика шарика

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import pygame
 
 
+class Ball:
+    def __init__(self, game_screen: pygame.Surface, coords: tuple, velocity=10, radius=20, color='white'):
+        self.screen = game_screen
+        self.x = coords[0]
+        self.y = coords[1]
+        self.vx = velocity / 2 ** 0.5
+        self.vy = velocity / 2 ** 0.5
+        self.radius = radius
+        self.color = color
+
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
+        if self.y - self.radius <= 0 or self.y + self.radius >= self.screen.get_height():
+            self.vy = -self.vy
+        if self.x - self.radius <= 0 or self.x + self.radius >= self.screen.get_width():
+            self.vx = -self.vx
+
+    def draw(self):
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+
+
 if __name__ == '__main__':
     pygame.init()
     size = (1200, 750)
