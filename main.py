61683e9bc4d8de0a2dcb8d8aa4d8eef7dda93e0d
bestcenny <52035479+bestcenny@users.noreply.github.com>
2022-01-12 01:14:20 +0300

    Добавление спрайта шара, отскакивание шара от платформмы

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
 import pygame
 
 
-class Ball:
-    def __init__(self, game_screen: pygame.Surface, coords: tuple, velocity=10, radius=20, color='white'):
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, game_screen: pygame.Surface, velocity=10):
+        super().__init__()
+        self.image = pygame.image.load('textures/ballGrey.png')
         self.screen = game_screen
-        self.x = coords[0]
-        self.y = coords[1]
+        self.rect = self.image.get_rect()
+        self.rect.center = (568, 600)
         self.vx = velocity / 2 ** 0.5
         self.vy = velocity / 2 ** 0.5
-        self.radius = radius
-        self.color = color
+        self.radius = self.image.get_rect()[3] // 2
+        print(self.image.get_rect()[3] // 2)
+        print(screen.get_height())
 
-    def move(self):
-        self.x += self.vx
-        self.y += self.vy
-        if self.y - self.radius <= 0 or self.y + self.radius >= self.screen.get_height():
-            self.vy = -self.vy
-        if self.x - self.radius <= 0 or self.x + self.radius >= self.screen.get_width():
-            self.vx = -self.vx
-
-    def draw(self):
-        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+    def update(self):
+        self.rect.x += self.vx
+        self.rect.y -= self.vy
+        if self.rect.y - self.radius <= 0:
+            self.vy = - self.vy
+        if self.rect.x <= 0 or self.rect.x + self.radius * 2 >= self.screen.get_width():
+            self.vx = - self.vx
+        if self.rect.y + self.radius * 2 >= self.screen.get_height():
+            self.kill()
+        if (paddle.get_coords_x() <= self.rect.x <= paddle.get_coords_x() + paddle.get_size()[2])\
+                and self.rect.y + self.radius * 2 >= paddle.get_coords_y():
+            self.vy = - self.vy
 
 
 class Paddle:
     # (self, game_screen: pygame.Surface, coord_x=568, coord_y=718,
 
     def __init__(self):
-        self.paddle_image = pygame.image.load('paddleRed.png')
+        self.paddle_image = pygame.image.load('textures/paddleRed.png')
         self.rect = self.paddle_image.get_rect()
         self.x = 568
         self.y = 718
@@ -50,11 +55,17 @@ class Paddle:
     def draw(self):
         screen.blit(self.paddle_image, (self.x, self.y))
 
+    def get_coords_x(self):
+        return self.x
+
+    def get_coords_y(self):
+        return self.y
+    def get_size(self):
+        return self.rect
+
 
 SIZE = (1200, 750)
 paddle = Paddle()
-
-
 
 if __name__ == '__main__':
     pygame.init()
@@ -62,10 +73,13 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
 
-    background_image = pygame.image.load('arkanoid back.jpg')
+    background_image = pygame.image.load('textures/arkanoid back.jpg')
     fps = 60
 
     running = True
+
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(Ball(screen))
 
     while running:  # Основной игровой цикл
         # обработка событий
@@ -75,9 +89,11 @@ if __name__ == '__main__':
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pass
 
+        all_sprites.update()
         paddle.move()
         # обновление экрана
         screen.blit(background_image, (0, 0))
+        all_sprites.draw(screen)
         paddle.draw()
         pygame.display.flip()
         clock.tick(fps)
